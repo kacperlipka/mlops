4a418b3d2216d5a7311773428d7fd04043f7375c
Kacper <kki@kmd.dk>
2024-06-06 13:36:06 +0200

    Configure IaC

---
+++ infrastructure/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "mlops"
+    storage_account_name = "infrastructure319068"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  } 
+}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,22 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
-    }
-  }
-
-  # Update this block with the location of your terraform state file
-  backend "azurerm" {
-    resource_group_name  = "mlops"
-    storage_account_name = "infrastructure319068"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_oidc             = true
-  }
-}
-
-provider "azurerm" {
-  features {}
-  use_oidc = true
+resource "azurerm_resource_group" "this" {
+  name = "mlops-test"
+  location = "West Europe"
 }

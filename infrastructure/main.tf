75ba0f09bd8de37218b3422e4e4eedebbe5f4df0
Kacper <kki@kmd.dk>
2024-06-06 19:32:06 +0200

    Update address_prefixes and subnet names in main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,28 +13,34 @@ data "azurerm_resource_group" "this" {
 
 locals {
   resource_group_name = var.resource_group.create ? azurerm_resource_group.this[0].name : data.azurerm_resource_group.this[0].name
+  backend_address_pool_name      = "${azurerm_virtual_network.this.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.this.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.this.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.this.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.this.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.this.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.this.name}-rdrcfg"
 }
 
 resource "azurerm_virtual_network" "this" {
   name                = "mlops-network"
   location            = var.location
   resource_group_name = local.resource_group_name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  address_space       = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "appgw" {
   name                 = "appgw-subnet"
   resource_group_name  = local.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.1.1.0/24"]
 }
 
 resource "azurerm_subnet" "kubernetes" {
   name                 = "kubernetes-kubernetes-subnet"
   resource_group_name  = local.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.1.2.0/24"]
 }
 
 resource "azurerm_dns_zone" "this" {
@@ -57,11 +63,11 @@ resource "azurerm_public_ip" "this" {
   allocation_method   = "Static"
 }
 
+resource "azurerm_application_gateway" "network" {
+  name                = "mlops-appgw"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
 
-resource "azurerm_application_gateway" "this" {
-  name                = "mlops-gateway"
-  resource_group_name = local.resource_group_name
-  location            = var.location
   sku {
     name     = "Standard_v2"
     tier     = "Standard_v2"
@@ -69,26 +75,26 @@ resource "azurerm_application_gateway" "this" {
   }
 
   gateway_ip_configuration {
-    name      = "mlops-ip-configuration"
-    subnet_id = azurerm_subnet.appgw.id
+    name      = "mlops-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.example.id
   }
 
   frontend_port {
-    name = "http"
+    name = local.frontend_port_name
     port = 80
   }
 
   frontend_ip_configuration {
-    name                 = "frontend-ip"
-    public_ip_address_id = azurerm_public_ip.this.ip_address
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.example.id
   }
 
   backend_address_pool {
-    name = "kubernetes"
+    name = local.backend_address_pool_name
   }
 
   backend_http_settings {
-    name                  = "http-setting"
+    name                  = local.http_setting_name
     cookie_based_affinity = "Disabled"
     path                  = "/"
     port                  = 80
@@ -97,19 +103,19 @@ resource "azurerm_application_gateway" "this" {
   }
 
   http_listener {
-    name                           = "http-listener"
-    frontend_ip_configuration_name = "frontend-ip"
-    frontend_port_name             = "http"
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
     protocol                       = "Http"
   }
 
   request_routing_rule {
-    name                       = "rule"
+    name                       = local.request_routing_rule_name
     priority                   = 9
     rule_type                  = "Basic"
-    http_listener_name         = "http-listener"
-    backend_address_pool_name  = "kubernetes"
-    backend_http_settings_name = "http-setting"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
   }
 }
 
@@ -129,7 +135,6 @@ resource "azurerm_kubernetes_cluster" "this" {
 
   network_profile {
     network_plugin = "azure"
-    service_cidr   = "10.0.1.0/16"
   }
 
   identity {

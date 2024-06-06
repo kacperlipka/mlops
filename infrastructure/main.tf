e0a2079e0d238cd18c5fa57709ed27ec89cc3b38
Kacper <kki@kmd.dk>
2024-06-06 18:10:16 +0200

    Delete overlapping address prefixes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_subnet" "kubernetes_nodes" {
   name                 = "kubernetes-nodes-subnet"
   resource_group_name  = local.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.3.0/24"]
 }
 
 resource "azurerm_dns_zone" "this" {

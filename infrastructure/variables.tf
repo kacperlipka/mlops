6b32f7ebc9586c3f7853bcaf99378b534c6a2fc8
Kacper <kki@kmd.dk>
2024-06-07 15:17:01 +0200

    Rename storage account

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -43,7 +43,7 @@ variable "storage_account" {
     nfs3_enabled             = bool
   })
   default = {
-    name                     = "mlopskubeflowstorageaccount"
+    name                     = "mlopskubeflowsa"
     account_tier             = "Standard"
     account_replication_type = "ZRS"
     is_hns_enabled           = true

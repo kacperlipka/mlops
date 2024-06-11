afc356fb0c42f8b9d51fa986e42bb3f9849f95f0
Kacper Lipka <kacperlipka@mbp-kacper.home>
2024-06-11 21:22:38 +0200

    Test CI

--- kubeflow/pipelines/main.py
+++ kubeflow/pipelines/main.py
@@ -1,5 +1,21 @@
+import sys
+import kfp
 from kfp import compiler
 from kfp import dsl
+import auth
+
+KUBEFLOW_ENDPOINT = "https://20.215.85.41"
+KUBEFLOW_USERNAME = "user@example.com"
+KUBEFLOW_PASSWORD = sys.argv[1]
+
+auth_session = auth.get_istio_auth_session(
+    url=KUBEFLOW_ENDPOINT,
+    username=KUBEFLOW_USERNAME,
+    password=KUBEFLOW_PASSWORD
+)
+
+client = kfp.Client(host=f"{KUBEFLOW_ENDPOINT}/pipeline", cookies=auth_session["session_cookie"], verify_ssl=False)
+
 
 
 @dsl.component
@@ -15,5 +31,4 @@ def my_pipeline():
   hello_world_task = print_hello_world()
 
 
-if __name__ == '__main__':
-  compiler.Compiler().compile(my_pipeline, 'pipeline.yaml')
+client.create_run_from_pipeline_func(my_pipeline, namespace="kubeflow-user-example-com")
